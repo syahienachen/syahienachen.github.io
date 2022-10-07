a4610efe17b57480686bb49eeeac3f7a4edaec34
syahienachen <syahienaputra313@gmail.com>
2022-10-07 16:27:27 +0700

    initial commit

--- src/router/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Outlet } from "react-router-dom";
-
-
-const PrivateRoute = () => {
-    return <Outlet/>
-}
-
-export default PrivateRoute
--- src/views/layouts/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import imgSpinner from "../../assets/images/logo512.png";
-
-const Spinner = () => (
-    <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: '80vh'}}>
-        <img src={imgSpinner} style={{ width:'200px', margin:'auto', display:'block' }} alt="Loading..." />
-    </div>
-)
-export default Spinner;
