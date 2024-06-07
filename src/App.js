38809415af6cfc72af0d669225942e0197776507
Putra Syahiena <syahiena.syaputra@salt.co.id>
2024-06-07 15:26:42 +0700

    add apa force 27

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ const App = () => {
     setIsOpenedAtAppTwo(isAppTwo())
 
     if (inAppTwo || (document.referrer !== '')) {
-      bukaDiChrome('https://syahienachen.vercel.app');
+      window.onload = () => {
+        bukaDiChrome('https://syahienachen.vercel.app');
+      }
       // window.location = "googlechrome://navigate?url=" + encodeURIComponent('https://syahienachen.vercel.app/');
     }
   }, [])
