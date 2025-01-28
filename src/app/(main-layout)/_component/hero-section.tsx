8974f22e6c0ba6510981be1fe5a2c3d0fc5b2ade
Putra Syahiena <syahiena.syaputra@salt.co.id>
2025-01-28 09:05:59 +0700

    add photo profil, change style

--- src/app/(main-layout)/_component/hero-section.tsx
+++ src/app/(main-layout)/_component/hero-section.tsx
@@ -6,8 +6,8 @@ export default function HeroSection() {
         <section className="py-3 px-4">
             <div className="text-lg font-bold mt-8">{`Hi, I'm Syahiena Maulana Syaputra!`}</div>
             <div className="text-3xl text-red-400 font-black mb-4">Front-End Developer</div>
-            <div className="text-xs font-normal dark:text-slate-950">I absolutely love building interactive and responsive web applications, with a strong focus on creating great user experiences.</div>
-            <div className="text-xs font-normal dark:text-slate-950 mt-1">My expertise includes React, Angular, TypeScript, as well as HTML, CSS, and JavaScript.</div>
+            <div className="text-xs font-normal">I absolutely love building interactive and responsive web applications, with a strong focus on creating great user experiences.</div>
+            <div className="text-xs font-normal mt-1">My expertise includes React, Angular, TypeScript, as well as HTML, CSS, and JavaScript.</div>
             <Image src="/hero-section.png" alt="hero-section-img" width={400} height={400} className="place-self-center"></Image>
             <div className="p-3 bg-pink-100 rounded">
                 <div className="text-xs font-semibold dark:text-slate-950 mb-3">Thanks for visiting my portfolio!</div>
